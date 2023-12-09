c75194a293c1f0a1a1ddf1203d9eaccab8beaa1d
程翔宇 <2867747371@qq.com>
2023-12-09 16:57:25 +0800

    testing title

--- Classes/StartMenuScene.cpp
+++ Classes/StartMenuScene.cpp
@@ -6,7 +6,8 @@ bool StartMenuScene::init()
 {
 	if (!Scene::init())return false;
 	//add sprites below
-
-
+	auto titile = Label::createWithTTF("Carrots", "fonts/arial.ttf", 50);
+	titile->setPosition(240, 200);
+	this->addChild(titile);
 	return true;
 }
